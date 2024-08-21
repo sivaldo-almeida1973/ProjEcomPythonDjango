e8d7df05ce5e5cfa30abe224c9273bcf6b6ad31c
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-08-21 11:43:43 -0300

    Criando uma classe de um modelo no Banco de Dados

--- ecommerce/loja/models.py
+++ ecommerce/loja/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+#importar a tabela do django
+from django.contrib.auth.models import User
 
 # Create your models here.
 # bases de dados(tabelas)
 
-#Cliente
-   # nome
-   #email
-   #telefone
-   #usuario
+class Cliente():
+   nome = models.CharField(max_length=200, null=True, blank=True)
+   email = models.CharField(max_length=200, null=True, blank=True)
+   telefone = models.CharField(max_length=200, null=True, blank=True)
+   id_sessao = models.CharField(max_length=200, null=True, blank=True) 
+   usuario = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE) # cada cliente so pode ter um uinico usuario e vice-versa
 
 #Produto
    # imagem
