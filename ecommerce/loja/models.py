6498fd7d33c5712fdef2cb71e4901198fc255fb1
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-08-20 21:35:27 -0300

    Modelagem do Banco de Dados

--- ecommerce/loja/models.py
+++ ecommerce/loja/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+# bases de dados(tabelas)
+
+#Cliente
+   # nome
+   #email
+   #telefone
+   #usuario
+
+#Produto
+   # imagem
+   # nome
+   # preco
+   # ativo
+   #categoria
+   # tipo
+
+
+
+# Categorias
+    #nome
+
+
+# Tipos 
+    #nome
+
+
+# ItemEstoque
+    # produto(ex: camisa)
+    # cor(ex: azul, laranja, verde)
+    # tamanho (ex: P, M, G)
+    # quantidade
+
+
+
+# ItensPedido
+    # itemestoque
+    # quantidade
+
+
+#Pedido
+    # cliente
+    # data_finalizacao
+    # finalizado
+    # id_transacao
+    # endereco
+    # itenspedido
+
+#Endereco 
+    # rua
+    # numero
+    # complemento
+    # cep
+    # cidade
+    # estado
+    # cliente
+
+
+
