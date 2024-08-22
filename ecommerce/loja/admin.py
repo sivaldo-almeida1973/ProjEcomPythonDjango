62df0db65848b28929daddeccd25f88fdb4bb905
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-08-22 09:23:30 -0300

    Criando os banners da homepage

--- ecommerce/loja/admin.py
+++ ecommerce/loja/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-# Cadastre seus modelos aqui(tabelas). que esta no models.py
+# rigistrar seus modelos aqui(tabelas). que esta no models.py
 admin.site.register(Cliente)
 admin.site.register(Categoria)
 admin.site.register(Tipo)
@@ -11,3 +11,4 @@ admin.site.register(ItemEstoque)
 admin.site.register(Endereco)
 admin.site.register(Pedido)
 admin.site.register(ItensPedido)
+admin.site.register(Banner)
